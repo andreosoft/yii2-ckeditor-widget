f9f4721dfa2dc0f06e89aead9904d29c37180836
andreosoft <AndreyZagorets@gmail.com>
2017-03-26 00:32:27 +0300

    Update Asset.php

--- Asset.php
+++ Asset.php
@@ -1,14 +1,18 @@
 <?php
-
 namespace andreosoft\ckeditor;
 
 use yii\web\AssetBundle;
 
 class Asset extends AssetBundle {
 
-    public $sourcePath = '@bower/ckeditor/dist';
+    public $sourcePath = '@bower/ckeditor';
     public $js = [
         'ckeditor.js',
+        'adapters/jquery.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
     ];
 
 }

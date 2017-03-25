dc11d7a08d6c013843a677b8c40900e0e8de0a72
andreosoft <AndreyZagorets@gmail.com>
2017-03-26 00:16:22 +0300

    Update CKEditor.php

--- CKEditor.php
+++ CKEditor.php
@@ -10,21 +10,38 @@ use yii\helpers\Url;
 class CKEditor extends InputWidget {
 
     public $editorOptions = [
+        
 ];
     public function init() {
         parent::init();
-        
-        /*
+
         $id = Json::encode('#'.$this->options['id']);
+        $imageIndex = Url::to(['/filemanager/image/index', 'extenalAction' => true]);
         $view = $this->getView();
-        $jsData = "(function() {"
-                . "var element = $($id);"
-                . "$(element).summernote(";
-//        $jsData .= empty($this->editorOptions) ? '' : (Json::encode($this->editorOptions));
-        $jsData .= $js;
-        */
-        $jsData = "CKEDITOR.replace($id);";
-        $view->registerJS($jsData);
+        
+        $jsOptions = empty($this->editorOptions) ? '{}' : (Json::encode($this->editorOptions));
+
+        $js = <<<JS
+        var element = $($id);
+//        $($id).ckeditor($jsOptions);
+        $($id).ckeditor(
+{  
+    filebrowserBrowseUrl: '$imageIndex',  
+    filebrowserUploadUrl: '$imageIndex' 
+});
+        $($id).ckeditor(function () {
+            this.addCommand('saveCommand', {
+                exec : function(editor, data) {
+                    element.text(editor.getData());
+                    unsaved = false;
+                    var form = element.closest('form');
+                    form.submit();
+                }
+            });
+            this.keystrokeHandler.keystrokes[CKEDITOR.CTRL + 83] = 'saveCommand';
+        });   
+JS;
+     $view->registerJS($js);   
         Asset::register($view);
     }
 
